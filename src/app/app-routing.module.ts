0ba93c068bb346ef9f8f5d3c74388441559b8f32
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2019-10-01 22:03:41 -0300

    Cabeçalho e CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '@app/dashboard/dashboard.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	// { path: ''} login
+	{ path: "dashboard", component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
