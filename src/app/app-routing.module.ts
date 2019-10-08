773c3af99bfb505bf0e074f3884443e690c839b4
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2019-10-07 23:00:04 -0300

    Começo da página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '@app/dashboard/dashboard.component';
+import { LoginComponent } from '@app/login/login.component';
 
 const routes: Routes = [
-	// { path: ''} login
-	{ path: "dashboard", component: DashboardComponent }
+	{ path: '', component: LoginComponent },
+	{ path: "dashboard", component: DashboardComponent },
 ];
 
 @NgModule({
